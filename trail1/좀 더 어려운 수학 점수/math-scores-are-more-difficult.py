0ad64a84a99be60764f2f5e1326f16e4ea0a24c4
GKwoo <go980314@gmail.com>
2026-09-12 23:43:11 +0900

    [Passed] 좀 더 어려운 수학 점수 (48ms, 16MB)

---
+++ trail1/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,9 @@
+A_math, A_eng = map(int, input().split())
+B_math, B_eng = map(int, input().split())
+
+if A_math > B_math:
+    print('A')
+elif (A_math == B_math) and A_eng > B_eng:
+    print('A')
+else:
+    print('B')    
